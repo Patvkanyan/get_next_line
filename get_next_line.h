bc8b6fa8e101921af61e3a8935fb8532df2a8846
Alen Patvakanyan <apatvaka@c2r9s5.42yerevan.am>
2025-02-08 15:13:23 +0400

    add

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: apatvaka <apatvaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 17:45:42 by alen              #+#    #+#             */
-/*   Updated: 2025/02/07 17:46:37 by apatvaka         ###   ########.fr       */
+/*   Updated: 2025/02/08 15:11:15 by apatvaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,5 @@
 char	*get_next_line(int fd);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 #endif
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: alen <alen@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 17:45:42 by alen              #+#    #+#             */
-/*   Updated: 2025/02/08 14:16:01 by alen             ###   ########.fr       */
+/*   Updated: 2025/02/08 15:11:39 by apatvaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,5 @@
 char	*get_next_line(int fd);
 char	*ft_strchr(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 #endif
